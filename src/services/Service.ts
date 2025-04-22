633fcb5bd91fa263b3140314bfd6f5117c315be2
Larissa <larissa.smata@hotmail.com>
2025-04-22 17:10:25 -0300

    Service

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -16,6 +16,7 @@ export const atualizar = async (url: string, dados: Object, setDados: Function) 
 export const listar = async (url: string, setDados: Function) => {
     const resposta = await api.get(url)
     setDados(resposta.data)
+    
 }
 
 export const deletar = async (url: string) => {
